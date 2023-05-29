20863098b7259b48a8f8e6cf4a6e2a891196c6c4
GeorgeDhz <1839279043@qq.com>
2023-05-29 22:52:38 +0800

    Site updated: 2023-05-29 22:52:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/demo0000.html","posts/java0000.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/demo0000.html","posts/java0001.html","posts/java0000.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
