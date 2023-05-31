d60bb6296d3a3f22c2e487097b80bcecc021d952
GeorgeDhz <1839279043@qq.com>
2023-05-31 19:20:28 +0800

    Site updated: 2023-05-31 19:20:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/demo0000.html","posts/c0010000.html","posts/c0010001.html","posts/c0010002.html","posts/a0090000.html","posts/a0090003.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/demo0000.html","posts/c0010000.html","posts/a0090000.html","posts/a0090003.html","posts/c0010002.html","posts/c0010001.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
