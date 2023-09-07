ddad6b41ef148fe4bb5ef8412645ce1d47aef518
DiHaozhe <113235544+DiHaozhe-365@users.noreply.github.com>
2023-09-07 16:51:04 +0800

    Site updated: 2023-09-07 16:51:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/34000/","posts/21000/","posts/34001/","posts/10000/","posts/31001/","posts/31000/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/10000/","posts/31001/","posts/31000/","posts/34001/","posts/34000/","posts/21000/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
